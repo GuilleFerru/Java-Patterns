f6abd5a865eb7d62edbffa716a7fbae92b69616c
GuilleFerru <guillelf@gmail.com>
2024-05-21 16:30:08 -0300

    new

--- src/App.java
+++ src/App.java
@@ -2,10 +2,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        Beverage beverage = new DarkRoast();
-        beverage.setSize("GRANDE");
-        beverage = new Mocha(beverage);
-        System.out.println(beverage.getSize() + " " + beverage.getDescription() + " $ " + beverage.cost());
+        
 
     }
 }
